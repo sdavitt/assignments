580ea6483c40c42ae68d07bfc23f84b5a6c8b569
Sam Davitt <sdavt2@gmail.com>
2020-08-10 12:03:53 -0400

    q1

--- prework.py
+++ prework.py
@@ -1,3 +1,11 @@
-def hello_world()
+#prework questions for Coding Temple course
 
-print("Hello, " + name)
+#Question 1 - print hello user
+def hello_name(user_name):
+    print("Hello, " + user_name)
+
+hello_name("Sam")
+
+#Question 2 - print the first 100 odd numbers
+def max_num_in_list(a_list):
+    
